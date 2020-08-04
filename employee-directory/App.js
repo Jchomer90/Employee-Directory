dd1622fa7c3488bffd109140a3f645a83c632e61
Josh Homer <joshuachomer@gmail.com>
2020-08-04 09:31:34 -0400

    npx install, added app.js component and api axios with render function

---
+++ employee-directory/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+import EmployeeTable from "./component/EmployeeTable/EmployeeTable"
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      data: []
+    }
+  }
+  componentDidMount(){
+    axios.get("https://randomuser.me/api/?results=200&nat=us")
+    .then (response => {
+      this.setState({data: response.data})
+    })
+  }
+  render(){
+    return (
+      // MY HTML FOR PAGE (MY REACT COMPONENTS)
+      <div className="container">
+        <EmployeeTable employees={this.state.data} />  
+      </div>
+    );
+  }
+}
+export default App;
---
+++ employee-directory/employee.js
@@ -0,0 +1,51 @@
+
+import React, { useState } from "react";
+import "./employeeTable.css";
+
+function EmployeeTable(props) {
+  const [input, setInput] = useState("");
+  const [willSortByAge, setWillSortByAge] = useState(false);
+  const storeInput = (e) => {
+
+    setInput(e.target.value);
+  };
+
+  let employees = (props.employees && props.employees.results) ? [...props.employees.results]:[];
+  console.log(employees, props.employees.results)
+  if(willSortByAge){
+      employees.sort(function (a,b){return a.dob.age-b.dob.age})
+  }
+  return (
+    <div>
+      <input onKeyUp={storeInput}></input>
+      <input onChange={radioAge} type="checkbox" name="options" id="option1" /> Age
+      <div className="row">
+        {console.log(props.employees)}
+        {employees
+            .filter((item) => {
+              let fullName = item.name.first + " " + item.name.last;
+              return fullName.toLowerCase().includes(input.toLowerCase());
+            })
+            .map((item) => {
+              return (
+                <div className="card employeeCard">
+                  <div className="card-body">
+                  <img src={item.picture.large} class="card-img-top" alt="..." />
+                    <h5 className="card-title">
+                      {item.name.first} {item.name.last}
+                    </h5>
+                    <p className="card-text">{item.cell}</p>
+                    <p className="card-text">{item.dob.age}</p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
+      </div>
+    </div>
+  );
+}
+
+export default EmployeeTable;
